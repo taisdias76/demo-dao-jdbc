62560a52e13c7945ffb622c49166aad2aa712383
Tais <taisdias76@gmail.com>
2022-09-06 23:29:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,8 @@ public class Program {
 		
 		Department department = new Department(1, "Books");
 		System.out.println(department);
-
+		Seller seller = new Seller(2, "Tais", "tais@gmail.com", new Date(), 3000.0, department);
+		System.out.println(seller);
 	}
 
 }
